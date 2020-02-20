0fdc6a04ada25a3bf1c204ce2af27f265a09f759
Geniorama <geniorama.info@gmail.com>
2020-02-20 18:17:24 -0500

    Iniciando proyecto

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    resolve: {
+        extensions: ['.js']
+    },
+    
+    entry: './src/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist'),
+    },
+
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            
+            { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                // Creates `style` nodes from JS strings
+                'style-loader',
+                // Translates CSS into CommonJS
+                'css-loader',
+                // Compiles Sass to CSS
+                'sass-loader',
+                ],
+            },
+        ]
+    }
+}
